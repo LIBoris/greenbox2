ed83267d9c00343045bbe03b3550ae7461a0d6dd
Tilek <dzhumanazarov_t@auca.kg>
2021-12-12 19:02:41 +0600

    greenbox2

---
+++ app/src/main/java/com/example/greenbox/MainActivitymenu.kt
@@ -0,0 +1,11 @@
+package com.example.greenbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitymenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activitymenu)
+    }
+}
